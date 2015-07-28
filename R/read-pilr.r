958ce7be99b06ef11a4dbe6025f4aa56397d90a0
tdschenk <schenk.tyler@gmail.com>
2015-07-28 15:17:06 -0500

    Add message indicating maximum rows were retrieved.

--- R/read-pilr.r
+++ R/read-pilr.r
@@ -15,22 +15,25 @@
 read_pilr <- function(pilr_server, project, data_set, schema, access_code,
                       bad_metadata_ok = FALSE, value = "parsed",
                       query_params = NULL, ...) {
-    pilr_server <- check_pilr_server(pilr_server)
-    project <- check_pilr_project(project)
-
-    if(!is.null(query_params)) {
-        query_string <- do.call(construct_query_string, query_params)
-    } else {
-        query_string <- ""
-    }
-
-    ep_data <- retrieve_pilr_data(pilr_server, project, data_set,
-                                  schema, query_string, access_code)
-
-    if(value != "parsed") {
-        return(ep_data)
-    }
-
-    json_to_pilr(ep_data, bad_metadata_ok)
-
+  pilr_server <- check_pilr_server(pilr_server)
+  project <- check_pilr_project(project)
+  
+  if(!is.null(query_params)) {
+    query_string <- do.call(construct_query_string, query_params)
+  } else {
+    query_string <- ""
+  }
+  
+  ep_data <- retrieve_pilr_data(pilr_server, project, data_set,
+                                schema, query_string, access_code)
+  
+  if(value != "parsed") {
+    return(ep_data)
+  }
+  
+  ret <- json_to_pilr(ep_data, bad_metadata_ok)
+  
+  if (nrow(ret) >= 10000) {
+    message("You have received the maximum capacity of 10000 data records. More records may exist in this dataset but must be retrieved in multiple parts.")
+  }
 }
