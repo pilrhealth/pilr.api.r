b0518d9242e7e843e08e4f78b922aad886f96708
tdschenk <schenk.tyler@gmail.com>
2016-11-14 12:59:59 -0600

    Fix for reading params

--- R/retrieve-pilr-params.r
+++ R/retrieve-pilr-params.r
@@ -1,7 +1,7 @@
 retrieve_pilr_params <- function(pilr_server, project, participant, instrument, period, access_code) {
   
-  endpoint <- paste0(pilr_server, "/api/v1", "/", project, "/",
-                     participant, "/", instrument, "/", period)
+  endpoint <- paste0(pilr_server, "/api/v1", "/", project, "/instrument/", instrument, "/participant/",
+                     participant, "/period/", period, "/variablesAndSettings")
   
   message("Attempting to get params from ", endpoint, " ...")
   
