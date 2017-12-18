5bbc89dd98cfb53c4bc0c66ee45baaa4180b2955
eruud <eruud@meinergy.com>
2017-12-18 14:19:33 -0600

    Update read-pilr.r

--- R/read-pilr.r
+++ R/read-pilr.r
@@ -33,5 +33,9 @@ read_pilr <- function(pilr_server, project, data_set, schema, access_code,
   
   ret <- json_to_pilr(ep_data, bad_metadata_ok)
   
+  if (nrow(ret) >= 10000) {
+    message("You have received the maximum capacity of 10000 data records. More records may exist in this dataset but must be retrieved in multiple parts.")
+  }
+  
   ret
 }
